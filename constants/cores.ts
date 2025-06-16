44cc8f30d2bd2672627434d3116a4734fce57a49
Marcos Henrique <marcosfhtrabalho@gmail.com>
2025-06-16 11:01:54 -0300

    feat: adição da paleta de cores principal

---
+++ constants/cores.ts
@@ -0,0 +1,18 @@
+
+interface Cores {
+    verde_principal: string,
+    verde_secundario: string,
+    preto: string,
+    branco: string,
+    cinza: string
+}
+
+const Cores: Cores = {
+    preto: '#131515',
+    branco: '#fffafb',
+    verde_principal: '#339989',
+    verde_secundario: '#7de2d1',
+    cinza: '#2b2c28'
+};
+
+export default Cores;
