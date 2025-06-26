c0bcd212bcf55ad99db2cc596df864dd884ba354
Marcos Henrique <161951682+MarcosHenriqueFr@users.noreply.github.com>
2025-06-25 22:31:50 -0300

    Update cores.ts

--- constants/cores.ts
+++ constants/cores.ts
@@ -1,11 +1,12 @@
-
 interface Cores {
     verde_principal: string,
     verde_secundario: string,
     preto: string,
     branco: string,
     cinza: string,
-    transparente: string
+    transparente: string,
+    degrade: string,
+    azul_link: string,
 }
 
 const Cores: Cores = {
@@ -14,7 +15,9 @@ const Cores: Cores = {
     verde_principal: '#339989',
     verde_secundario: '#7de2d1',
     cinza: '#2b2c28',
-    transparente: 'rgba(0, 0, 0, 0.0)'
+    transparente: 'rgba(0, 0, 0, 0.0)',
+    degrade: '#11332E',
+    azul_link: '#63ADE5',
 };
 
 export default Cores;
