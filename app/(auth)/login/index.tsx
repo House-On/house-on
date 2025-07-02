9f95b76d1b2ea74dee1a058beb86e9f1b3c332c2
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-07-02 12:45:35 -0300

    fix: reestruturação das telas em componentes

---
+++ app/(auth)/login/index.tsx
@@ -0,0 +1,5 @@
+import Login from "@/components/login_page/login";
+
+export default function rotaLogin(){
+    return <Login />;
+}
---
+++ app/(main)/perfil/index.tsx
@@ -0,0 +1,5 @@
+import Perfil from "@/components/perfil_page/perfil";
+
+export default function rotaPerfil(){
+    return <Perfil />;
+}
