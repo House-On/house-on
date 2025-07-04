2a8f068f7097aa45b98144760cfd19fcc3666f1b
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-07-03 21:10:18 -0300

    fix: remove o link da tela de automações

--- app/(main)/(tabs)/auto.tsx
+++ app/(main)/(tabs)/auto.tsx
@@ -1,13 +1,11 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-const Automacoes = () => {
+const Auto = () => {
     return (
         <View>
             <Text>Essa é a tela de Automações</Text>
-            <Link href={"/LoginPs"}>TELA NOVA</Link>
         </View>
     );
 }
 
-export default Automacoes;
+export default Auto;
